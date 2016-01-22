7bf895e441b7719c7030d912a72faa4bb986eeac
dalong <lxr9161@sina.com>
2016-01-22 17:15:04 +0800

    v5

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 
 	public function __construct(){
 		parent::__construct();
-		$this->user = M('user');
+		$this->user = D('user');
 	}
     public function index(){
 		$this->display('');
@@ -22,20 +22,34 @@ class UserController extends Controller
 		$this->display('login');
 	}
 
+	public function loginPost(){
+		if(IS_POST){
+			
+		}
+	}
+
 	public function register(){
 		$this->display('register');
 	}
 	public function registerPost(){
 		if(IS_POST){
-			$data = $this->user->create();
+			$data['user_name'] = $_POST['user_name'];
+			$data['user_password'] = sha1($_POST['user_password']);
+			$data['user_password_ok'] = sha1($_POST['user_password_ok']);
 			$data['user_create_time'] = date('Y-m-d H:i:s',time());
 			$data['user_last_login_time'] = $data['user_create_time'];
 			$data['user_last_login_ip'] = $_SERVER['REMOTE_ADDR'];
-			if($this->user->add($data)){
-				$this->success('注册成功','/user');
+			if(!$this->user->create()){
+
+				exit($this->user->getError());
 			}else{
-				$this->error('注册失败','/register');
+				if($this->user->add($data)){
+					$this->success('注册成功','/user');
+				}else{
+					$this->error('注册失败','/register');
+				}
 			}
+			
 		}
 	}
 }
